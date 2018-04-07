6ec0291e5cadcb7c01c54a7c45ee8c64551b9f66
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-04-07 19:30:35 +0300

    add(*): init components for start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ShopListComponent } from './components/shop-list/shop-list.component';
+import { AdviceComponent } from './components/advice/advice.component';
+import { SubscriptionComponent } from './components/subscription/subscription.component';
+import { MainBannerComponent } from './components/main-banner/main-banner.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IndexPageComponent } from './components/index-page/index-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShopListComponent,
+    AdviceComponent,
+    SubscriptionComponent,
+    MainBannerComponent,
+    BrandsComponent,
+    ArticlesComponent,
+    IndexPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
