7c3df2f0ccd2df48c6ef9ae3b2d6d05cf10b88c4
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-07-14 23:44:42 +0300

    update(page-catalog): add filter template

--- src/app/components/page-catalog/page-catalog.component.ts
+++ src/app/components/page-catalog/page-catalog.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-page-catalog',
-  templateUrl: './page-catalog.component.html',
-  styleUrls: ['./page-catalog.component.css']
+    selector: 'app-page-catalog',
+    templateUrl: './page-catalog.component.html',
+    styleUrls: ['./page-catalog.component.css']
 })
 export class PageCatalogComponent implements OnInit {
+    @ViewChild("tref", {read: ElementRef}) tref: ElementRef;
 
-  constructor() { }
+    filterCategories = {
+        categories: false,
+        price: false,        
+        size: false,
+        brand: false,
+        colour: false
+    };
+    constructor(protected elementRef: ElementRef) { }
+    
+    toggleCategory(name){
+        for (let category in this.filterCategories){           
+            this.filterCategories[category] = (name !== category) ? false : true; 
+        }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
