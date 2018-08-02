4483b8c510940db549cc8c593e4e176683d8fbdb
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-08-02 23:57:45 +0300

    add(page-index): create module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,57 +11,43 @@ import { GoodsService } from './services/goods/goods.service';
 import { SharedService } from './services/shared/shared.service';
 import { DataService } from './services/data/data.service';
 
+import { PageIndexModule } from './modules/page-index/page-index.module';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ShopListComponent } from './components/shop-list/shop-list.component';
-import { AdviceComponent } from './components/advice/advice.component';
-import { SubscriptionComponent } from './components/subscription/subscription.component';
-import { MainBannerComponent } from './components/main-banner/main-banner.component';
-import { BrandsComponent } from './components/brands/brands.component';
-import { ArticlesComponent } from './components/articles/articles.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PageCatalogComponent } from './components/page-catalog/page-catalog.component';
 import { PageIndexComponent } from './components/page-index/page-index.component';
+import { PageAboutComponent } from './components/page-about/page-about.component';
+import { PageContactComponent } from './components/page-contact/page-contact.component';
+import { PageShopIdComponent } from './components/page-shop-id/page-shop-id.component';
 import { SignupComponent } from './components/user/signup/signup.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
 import { SigninComponent } from './components/user/signin/signin.component';
 import { ShoppingCartComponent } from './components//shopping-cart/shopping-cart.component';
-import { PageAboutComponent } from './components/page-about/page-about.component';
-import { PageContactComponent } from './components/page-contact/page-contact.component';
-import { PageShopIdComponent } from './components/page-shop-id/page-shop-id.component';
-
 
 @NgModule({
 	declarations: [
-		AppComponent,
-		HeaderComponent,
-		FooterComponent,
-		ShopListComponent,
-		AdviceComponent,
-		SubscriptionComponent,
-		MainBannerComponent,
-		BrandsComponent,
-		ArticlesComponent,
+		AppComponent,		
 		PageCatalogComponent,
 		PageIndexComponent,
+		PageAboutComponent,
+		PageContactComponent,
+		PageShopIdComponent,
 		SignupComponent,
 		ProfileComponent,
 		SigninComponent,
-		ShoppingCartComponent,
-		PageAboutComponent,
-		PageContactComponent,
-		PageShopIdComponent
+		ShoppingCartComponent		
 	],
 	imports: [
 		BrowserModule,
-		FormsModule,
+		FormsModule,		
 		AppRoutingModule,
+		PageIndexModule,
 		HttpModule,
 		HttpClientModule,
 		HttpClientInMemoryWebApiModule.forRoot(
 			InMemoryDataService
-		)
+		)		
 	],
 	providers: [HttpClientModule, SharedService, DataService, GoodsService],
 	bootstrap: [AppComponent]
---
+++ src/app/modules/page-index/page-index.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from '../../app-routing.module';
+
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { ShopListComponent } from '../../components/shop-list/shop-list.component';
+import { AdviceComponent } from '../../components/advice/advice.component';
+import { SubscriptionComponent } from '../../components/subscription/subscription.component';
+import { MainBannerComponent } from '../../components/main-banner/main-banner.component';
+import { BrandsComponent } from '../../components/brands/brands.component';
+import { ArticlesComponent } from '../../components/articles/articles.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        HeaderComponent,
+        FooterComponent,
+        ShopListComponent,
+        AdviceComponent,
+        SubscriptionComponent,
+        MainBannerComponent,
+        BrandsComponent,
+        ArticlesComponent
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent,
+        ShopListComponent,
+        AdviceComponent,
+        SubscriptionComponent,
+        MainBannerComponent,
+        BrandsComponent,
+        ArticlesComponent        	
+    ]
+})
+export class PageIndexModule { }
