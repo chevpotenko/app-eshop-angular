0f16c915ea5e9f2efefc20ee4b89ab835f17a617
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-09-19 20:54:00 +0300

    update(checkout): add table

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule  } from '../shared/shared.module';
+
 import { checkoutRouting } from './checkout.routing';
 import { PageCheckoutComponent } from './pages/checkout/checkout.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
+		SharedModule,
 		checkoutRouting
 	],
 	declarations: [
--- src/app/modules/checkout/pages/checkout/checkout.component.ts
+++ src/app/modules/checkout/pages/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'app-checkout',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./checkout.component.css']
 })
 export class PageCheckoutComponent implements OnInit {
+	
+    public shoppingcart;
 
-	constructor() { }
+	constructor(private route: ActivatedRoute) {
 
-	ngOnInit() {
 	}
 
+	ngOnInit() {
+		this.route.params.subscribe((params) => {
+			this.shoppingcart = [{
+				product: {
+					id: 1,
+					name: params.name,
+					category: "",
+					price: params.price,
+					rating: "",
+					discount: "",
+					img: ""
+				},
+				id: 1,
+				qty: params.quantity,
+				total: parseInt(params.quantity) * parseInt(params.price)
+			}];
+		})
+		
+	}
 }
