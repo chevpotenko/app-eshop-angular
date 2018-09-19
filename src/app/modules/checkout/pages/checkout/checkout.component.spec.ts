b26928760cd5e6fe20a29e8bf6c1e8b6984136da
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2018-09-19 21:00:35 +0300

    update(checkout.component.spec): update class

--- src/app/modules/checkout/pages/checkout/checkout.component.spec.ts
+++ src/app/modules/checkout/pages/checkout/checkout.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CheckoutComponent } from './checkout.component';
+import { PageCheckoutComponent } from './checkout.component';
 
-describe('CheckoutComponent', () => {
-  let component: CheckoutComponent;
-  let fixture: ComponentFixture<CheckoutComponent>;
+describe('PageCheckoutComponent', () => {
+  let component: PageCheckoutComponent;
+  let fixture: ComponentFixture<PageCheckoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CheckoutComponent ]
+      declarations: [ PageCheckoutComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CheckoutComponent);
+    fixture = TestBed.createComponent(PageCheckoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
