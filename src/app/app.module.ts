e7d1a8502c7970f82496174a8525c98757348568
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-02-19 14:48:33 +0200

    update(page-shop-id): page test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './services/in-memory-data/in-memory-data.service';
+import { InMemoryDataService } from './services/in-memory-data/in-memory-data.service';
 
 import { UserService } from './services/user/user.service';
 import { DataService } from './services/data/data.service';
 import { ShopService } from './services/shop/shop.service';
+
 
 import { SharedModule } from './modules/shared/shared.module';
 import { PageIndexModule } from './modules/index/index.module';
@@ -33,24 +34,22 @@ import { routing } from './app-routing.module';
 		ShoppingCartComponent
 	],
 	imports: [
-		BrowserModule,		
+		BrowserModule,
 		FormsModule,
 		SharedModule,
 		PageIndexModule,
 		ShopModule,
 		routing,
-		HttpModule,		
+		HttpModule,
 		HttpClientModule,
-		HttpClientInMemoryWebApiModule.forRoot(
-			InMemoryDataService
-		)		
+		HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
 	],
 	providers: [
 		HttpClientModule,
 		DataService,
 		UserService,
 		ShopService
-	],	
+	],
 	bootstrap: [AppComponent]
 })
 
