f6f9298d88437d2d5c4d32e00b616b9913e9a8e2
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-03-14 21:06:38 +0200

    add(auth-guard.service)

---
+++ src/app/services/auth-guard/auth-guard.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) { }
+
+    canActivate() {
+        if (this.authService.isLoggedIn()) {
+            this.router.navigate(['user/profile']);
+        }
+        return !this.authService.isLoggedIn();
+    }
+}
