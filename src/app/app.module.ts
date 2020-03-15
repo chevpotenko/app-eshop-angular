c12d45f5dccdfc5c7075a20a8f43aa4306fe107b
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-03-15 17:50:40 +0200

    refactor(app): app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
-
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClientXsrfModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './services/in-memory-data/in-memory-data.service';
-
-import { UserService } from './services/user/user.service';
-import { DataService } from './services/data/data.service';
-import { ShopService } from './services/shop/shop.service';
-
+import { environment } from '../environments/environment';
 
 import { SharedModule } from './modules/shared/shared.module';
 import { PageIndexModule } from './modules/index/index.module';
 import { ShopModule } from './modules/shop/shop.module';
+import { routing } from './app-routing.module';
 
+import { UserService } from './services/user/user.service';
+import { DataService } from './services/data/data.service';
+import { ShopService } from './services/shop/shop.service';
+import { AuthService } from './services/auth/auth.service';
+import { AuthGuardService } from './services/auth-guard/auth-guard.service';
+import { ProfileGuardService } from './services/profile-guard/profile-guard.service';
+import { TokenInterceptorService } from './services/token-interceptor/token-interceptor.service';
+import { InMemoryDataService } from './services/in-memory-data/in-memory-data.service';
+
+import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './modules/shoppingcart/pages/shoppingcart/shopping-cart.component';
 import { SignupComponent } from './modules/user/pages/signup/signup.component';
 import { SigninComponent } from './modules/user/pages/signin/signin.component';
 import { ProfileComponent } from './modules/user/pages/profile/profile.component';
-
-import { AppComponent } from './app.component';
-
-import { routing } from './app-routing.module';
-
-import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -38,24 +36,30 @@ import { environment } from '../environments/environment';
     imports: [
         BrowserModule,
         FormsModule,
-        SharedModule,
-        PageIndexModule,
-        ShopModule,
-        routing,
-        HttpModule,
         HttpClientModule,
         HttpClientXsrfModule.withOptions({
-            cookieName: 'XSRF-TOKEN-Cookie',
-            headerName: 'XSRF-TOKEN-Header',
+            cookieName: 'XSRF-TOKEN',
+            headerName: 'X-XSRF-TOKEN',
         }),
-        environment.production ?
-            [] : HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
+        environment.production ? [] : HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
+        routing,
+        SharedModule,
+        ShopModule,
+        PageIndexModule
     ],
     providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterceptorService,
+            multi: true
+        },
         HttpClientModule,
         DataService,
         UserService,
-        ShopService
+        ShopService,
+        AuthGuardService,
+        ProfileGuardService,
+        AuthService
     ],
     bootstrap: [ AppComponent ]
 })
