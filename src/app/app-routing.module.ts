9ee21e74d468f6186cb7c127ba7583ff9064c496
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-03-15 18:09:24 +0200

    update(app-routing.module.ts): add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,35 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
+
+import { ProfileGuardService } from './services/profile-guard/profile-guard.service';
+import { AuthGuardService } from './services/auth-guard/auth-guard.service';
+
 import { PageIndexComponent } from './modules/index/pages/index/page-index.component';
 import { PageCatalogComponent } from './modules/shop/pages/catalog/page-catalog.component';
 import { PageShopIdComponent } from './modules/shop/pages/id/page-shop-id.component';
+import { ShoppingCartComponent } from './modules/shoppingcart/pages/shoppingcart/shopping-cart.component';
 import { SignupComponent } from './modules/user/pages/signup/signup.component';
 import { SigninComponent } from './modules/user/pages/signin/signin.component';
 import { ProfileComponent } from './modules/user/pages/profile/profile.component';
-import { ShoppingCartComponent } from './modules/shoppingcart/pages/shoppingcart/shopping-cart.component';
 
 const routes: Routes = [
     { path: 'catalog', component: PageCatalogComponent },
     { path: 'catalog/:id', component: PageCatalogComponent },
     { path: 'products/:id', component: PageShopIdComponent },
-    { path: 'user/signup', component: SignupComponent },
-    { path: 'user/signin', component: SigninComponent },
-    { path: 'user/profile', component: ProfileComponent },
-    { path: 'user/logout', component: PageIndexComponent },
     { path: 'shoppingcart', component: ShoppingCartComponent },
+    { path: 'user/signup', component: SignupComponent },
+    {
+        path: 'user/signin',
+        component: SigninComponent,
+        canActivate: [ AuthGuardService ]
+    },
+    {
+        path: 'user/profile',
+        component: ProfileComponent,
+        canActivate: [ ProfileGuardService ],
+        canLoad: [ ProfileGuardService ]
+    },
+    { path: 'user/logout', component: PageIndexComponent },
     { path: 'checkout', loadChildren: './modules/checkout/checkout.module#PageCheckoutModule' },
     { path: 'contact', loadChildren: './modules/contact/contact.module#PageContactModule' },
     { path: 'about', loadChildren: './modules/about/about.module#PageAboutModule' },
