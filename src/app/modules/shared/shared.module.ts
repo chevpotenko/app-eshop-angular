51c847d8e83d03efa48c81d39ced8015f6921e39
Oleksiy Chevpotenko <ochevpotenko@gmail.com>
2020-08-25 09:06:24 +0300

    refactor(shared.module.ts)

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { ShopListComponent } from './shop-list/shop-list.component';
 import { ShoppingcartTableComponent } from './shoppingcart-table/shoppingcart-table.component';
 
 @NgModule({
     declarations: [
+        HeaderComponent,
+        FooterComponent,
         ShopListComponent,
         ShoppingcartTableComponent
     ],
@@ -14,8 +18,11 @@ import { ShoppingcartTableComponent } from './shoppingcart-table/shoppingcart-ta
         RouterModule
     ],
     exports: [
+        HeaderComponent,
+        FooterComponent,
         ShopListComponent,
         ShoppingcartTableComponent
     ]
 })
+
 export class SharedModule { }
